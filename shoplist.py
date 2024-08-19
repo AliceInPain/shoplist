f6fcfaa50f23dab393d291ec5417a6c84f82dc32
AliceInPain <nazaninshafaei.ns@gmail.com>
2024-08-19 12:29:26 +0330

    cleaned

--- shoplist.py
+++ shoplist.py
@@ -14,7 +14,7 @@ for key, value in data.items():
             user_order = user["orders"]
             user_name = user["name"]
             if user_order:
-                print(user_name)  # displays --> John Do Jane Smith
+                print(user_name) # displays --> John Doe Jane Smith
 
 
 for key, value in data.items():
@@ -22,11 +22,17 @@ for key, value in data.items():
         for user in value:
             user_order = user["orders"]
             user_name = user["name"]
+            print(f"User: {user_name}")
             for order in user_order:
                 user_amount = order["amount"]
                 user_product = order["product"]
                 print(f"Product: {user_product}, Amount: {user_amount}")
-
+                #displays:
+                # User: John Doe
+                # Product: Laptop, Amount: 1200.5
+                # Product: Smartphone, Amount: 800.75
+                # User: Jane Smith
+                # Product: Tablet, Amount: 300.0
 
 for key, value in data.items():
     if key == "users":
@@ -38,3 +44,10 @@ for key, value in data.items():
                 product_name = order["product"]
                 product_amount = order["amount"]
                 print(f"   Product: {product_name}, Amount: ${product_amount}")
+                
+                #displays:
+                #User: John Doe
+                #     Product: Laptop, Amount: $1200.5
+                #     Product: Smartphone, Amount: $800.75
+                # User: Jane Smith
+                #     Product: Tablet, Amount: $300.0
