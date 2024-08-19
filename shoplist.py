21dc5cade6a7b20325ecd39fd93702d76b4a0e47
AliceInPain <nazaninshafaei.ns@gmail.com>
2024-08-19 12:09:57 +0330

    json data imported and cleaned

--- shoplist.py
+++ shoplist.py
@@ -8,14 +8,6 @@ with open("file.json", mode="r", encoding="utf-8") as read_file:
 
 # USERS is a list of dictionaries
 
-
-# name of the users who ordered
-# users_with_order = []  # storing the names of users who purchased a product
-# for user in data["users"]:  # []--> to access a key in a dictionary
-#     if user["orders"]:
-#         users_with_order.append(user["name"])
-# pprint(users_with_order)  # displays --> ['John Doe', 'Jane Smith']
-
 for key, value in data.items():
     if key == "users":
         for user in value:
@@ -24,13 +16,6 @@ for key, value in data.items():
             if user_order:
                 print(user_name)  # displays --> John Do Jane Smith
 
-# list of products + amount
-# list_of_products = []
-# for user in data["users"]:
-#     for order in user["orders"]:
-#         list_of_products.append((order["product"], order["amount"]))
-# # displays --> [('Laptop', 1200.5), ('Smartphone', 800.75), ('Tablet', 300.0)]
-# pprint(list_of_products)
 
 for key, value in data.items():
     if key == "users":
@@ -42,23 +27,6 @@ for key, value in data.items():
                 user_product = order["product"]
                 print(f"Product: {user_product}, Amount: {user_amount}")
 
-# # list of products + name and amount
-# orders_info = {}
-# for user in data["users"]:
-#     # print(user["name"])
-#     if user["name"] not in orders_info:
-#         orders_info[user["name"]] = []
-
-#     for order in user["orders"]:
-#         user_details = {"Product": order["product"], "Amount": order["amount"]}
-#         orders_info[user["name"]].append(user_details)
-# # print(orders_info) #--> [{'User': 'John Doe', 'Product': 'Laptop', 'Amount': 1200.5}, {'User': 'John Doe', 'Product': 'Smartphone', 'Amount': 800.75}, {'User': 'Jane Smith', 'Product': 'Tablet', 'Amount': 300.0}]
-
-
-# for user, item in orders_info.items():
-#     print(f"User:{user}")
-#     for order in item:
-#         print(f"    Product: {order["Product"]},\n    Amount: ${order["Amount"]}")
 
 for key, value in data.items():
     if key == "users":
